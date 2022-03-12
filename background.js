f56796141923b2e376e93706f6881c3d1d2850f7
karl-pae <83178186+karl-pae@users.noreply.github.com>
2022-03-12 06:39:26 +0530

    Update background.js
    Modified and added major changes.

--- background.js
+++ background.js
@@ -1,11 +1,26 @@
+//This serves two functions. Restart after tabs are closed or click on extension to restart session.
 var count = 0;
+
+const closeAllTabs = ()=>{
+	chrome.tabs.create({ url: 'chrome://bookmarks' }, function (newTab) {
+		let querying = chrome.tabs.query({}, function (tabs) {
+			for (let tab of tabs) {
+				if (tab.id !== newTab.id) chrome.tabs.remove(tab.id);
+			}
+		});
+	});
+}
+
+chrome.action.onClicked.addListener(function (thisTab) {
+	closeAllTabs();
+});
 
 chrome.tabs.onRemoved.addListener(() => {
 chrome.tabs.query({currentWindow: true}, (tabs) => {
 
   if( tabs.length === 0 && count === 0){
 	count = 1;
-	chrome.tabs.create({ url: 'chrome-search://local-ntp/local-ntp.html' });
+	chrome.tabs.create({ url: 'chrome://bookmarks' });
 	setTimeout(function(){ count = 0; }, 500);
   }
 })
